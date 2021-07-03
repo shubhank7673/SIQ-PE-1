34d40b5a7a3adf58126ed63b0ed3379c327112cd
ShubhankKhare <>
2021-07-03 05:18:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Slot{
+
+};
+class Parkinglot{
+  public:
+    vector<Slot*>slots;
+    // unordered_map<>
+};
+// Main driver code
+int main() {
+
+}
